59ef73f0edea3d5ff7f111bfb65e63b4b68db669
jojo11h <148336324+jojo11h@users.noreply.github.com>
2023-10-31 09:25:20 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -1,28 +1,32 @@
-import requests as r
+from requests import get
 from dotenv import load_dotenv
-import os
+from os import getenv
 
 load_dotenv()
 
 
 def get_goe_city(city):
-    url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={os.getenv("WEATHER_APP_KEY")}'
-    result = (r.get(url)).json()
-    lat = result[1]['lat']
-    lon = result[1]['lon']
+    url = f'http://api.openweathermap.org/geo/1.0/direct?q={
+        city}&limit=5&appid={getenv("WEATHER_APP_KEY")}'
+    result = get(url).json()
+    lat = result[1]["lat"]
+    lon = result[1]["lon"]
     return lat, lon
 
 
 def weather_city(city_geo):
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={city_geo[0]}&lon={city_geo[1]}&appid={os.getenv("WEATHER_APP_KEY")}'
-    result = r.get(url).json()
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={
+        city_geo[0]}&lon={city_geo[1]}&appid={getenv("WEATHER_APP_KEY")}'
+    result = get(url).json()
     return result["weather"]
 
 
 user_search1 = "La Rochelle"
-user_search2 = 'Dunkerque'
+user_search2 = "Dunkerque"
+
+print(get_goe_city(user_search1))
 
 geo1 = get_goe_city(user_search1)
 geo2 = get_goe_city(user_search2)
-print('La Rochelle => ', weather_city(geo1))
-print('Dunkerque => ', weather_city(geo2))
+print("La Rochelle => ", weather_city(geo1))
+print("Dunkerque => ", weather_city(geo2))
